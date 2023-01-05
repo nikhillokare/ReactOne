d2c166ae285df86c44ea37cadcecdac224cf7ba2
Nikhil <nikhillokare6@gmail.com>
2023-01-05 23:16:07 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+     <div>
+        <h1>TechVoid</h1>
+        <p>@All Right Reserved</p>
+     </div>
+     <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://www.youtube.com/channel/UCLHYo-NdyC9xQfazxKaM3Ow" target={"blank"}>Youtube</a>
+            <a href="https://twitter.com/NikhilLokare12" target={"blank"}>Twitter</a>
+            <a href="https://www.instagram.com/nikkk___24/" target={"blank"}>Instagram</a>
+            <a href="https://github.com/nikhillokare" target={"blank"}>Github</a>
+        </div>
+     </div>
+    </footer>
+  )
+}
+
+export default Footer
